ab64e1fefb0b0acbeeb516d9f0131012dae76584
Ariana <arianana.quibre@gmail.com>
2025-05-16 09:13:13 -0600

    Primer commit

---
+++ backendp2p4/src/main/java/org/example/backendp2p4/Backendp2p4Application.java
@@ -0,0 +1,13 @@
+package org.example.backendp2p4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backendp2p4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backendp2p4Application.class, args);
+    }
+
+}
---
+++ backendp2p4/src/test/java/org/example/backendp2p4/Backendp2p4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendp2p4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backendp2p4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
